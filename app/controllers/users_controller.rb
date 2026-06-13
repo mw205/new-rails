9eb890c34f4c6ca1f1ac8efc63fbaa6f964ccd14
Mohamed Waleed <mw99746@gmail.com>
2026-06-13 14:45:35 +0300

    Implement UsersController CRUD actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,56 @@
 class UsersController < ApplicationController
-  
+  before_action :set_user, only: %i[ show edit update destroy ]
+
+  # GET /users
+  def index
+    @users = User.all
+  end
+
+  # GET /users/:id
+  def show
+  end
+
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+
+  # GET /users/:id/edit
+  def edit
+  end
+
+  # POST /users
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user, notice: "User was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /users/:id
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: "User was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /users/:id
+  def destroy
+    @user.destroy!
+    redirect_to users_url, notice: "User was successfully deleted."
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :dob, :email, :phone_number, :address)
+  end
 end
